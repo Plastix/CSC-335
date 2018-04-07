7c41d2ec8a209eebdddd9234277222533cc80eb6
Plastix <aidanpieper@gmail.com>
2018-04-07 10:06:43 -0400

    run.sh: Only boot if copilation succeeds

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
-./compile.sh
-./boot.sh $@
+cd src/kern/compile/PROJ1
+bmake depend
+bmake
+#bmake WERROR=
+if [ $? -eq 0 ]; then
+    bmake install
+    cd ../../../..
+    ./boot.sh $@
+fi
